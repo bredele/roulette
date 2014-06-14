69e42be632d8e451b05e3a9e6deadef746b2c6ef
bredele <olivier.wietrich@gmail.com>
2014-06-14 07:01:53 -0600

    prepare tests

---
+++ test/roulette.js
@@ -0,0 +1,27 @@
+
+/**
+ * Test dependencies.
+ */
+
+var assert = require('assert');
+var roulette = require('..');
+
+describe('switch positions', function() {
+	
+	it('should do nothing if just one element', function() {
+		
+	});
+
+	it('should switch 2 elements', function() {
+	});
+
+	it('should switch 3 elements', function() {
+
+	});
+
+	it('should switch multiple elements', function() {
+
+	});
+
+	
+});
