3e80a45239d8d4e23e729659dc2799d56ce7306e
bredele <olivier.wietrich@gmail.com>
2014-06-14 07:07:34 -0600

    setup tests

--- test/roulette.js
+++ test/roulette.js
@@ -8,8 +8,19 @@ var roulette = require('..');
 
 describe('switch positions', function() {
 	
+	var parent, el1, el2;
+	beforeEach(function() {
+		parent = document.createElement('div');
+		el1 = document.createElement('div');
+		el1.innerHTML = 'el1';
+		el2 = document.createElement('div');
+		el2.innerHTML = 'el2';
+		parent.appendChild(el1);
+		parent.appendChild(el2);
+	});
+	
 	it('should do nothing if just one element', function() {
-		
+		roulette(el1);
 	});
 
 	it('should switch 2 elements', function() {
