3be83befa70131a0a77ce5c204e68019103adcda
bredele <olivier.wietrich@gmail.com>
2014-06-22 15:15:39 -0600

    test roulette with more than 2 elements

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ module.exports = roulette;
  */
 
 function roulette(el1, el2) {
-	// NOTE: dom is synchronous so it could be global
-	var anchor = document.createElement('div');
-	insert(el2, anchor);
-	insert(el1, el2);
-	insert(anchor, el1);
-	anchor.parentElement.removeChild(anchor);
+  var anchor = document.createElement('div');
+  insert(el2, anchor);
+  insert(el1, el2);
+  insert(anchor, el1);
+  anchor.parentElement.removeChild(anchor);
 }
 
 
@@ -33,5 +32,5 @@ function roulette(el1, el2) {
  */
 
 function insert(parent, child) {
-	parent.insertAdjacentElement('beforebegin', child);
+  parent.insertAdjacentElement('beforebegin', child);
 }
--- test/roulette.js
+++ test/roulette.js
@@ -8,28 +8,34 @@ var roulette = require('roulette');
 
 describe('switch positions', function() {
 	
-	var parent, el1, el2;
+	var parent, el1, el2, el3;
 	beforeEach(function() {
 		parent = document.querySelector('.parent');
+		parent.innerHTML = '<div class="el1">el1</div><div class="el2">el2</div><div class="el3">el3</div>';
 		el1 = document.querySelector('.el1');
 		el2 = document.querySelector('.el2');
+		el3 = document.querySelector('.el3');
 	});
-	
+
 	// it('should do nothing if just one element', function() {
 	// 	roulette(el1);
 	// 	assert.equal(parent.firstChild, el1);
 	// });
 
 	it('should switch 2 elements', function() {
-		roulette(el1, el2);
 		var children = parent.children;
+		roulette(el1, el2);
 		assert.equal(children[0], el2);
 		assert.equal(children[1], el1);
 	});
 
-	// it('should switch 3 elements', function() {
-
-	// });
+	it('should switch 3 elements', function() {
+		var children = parent.children;
+		roulette(el1, el2, el3);
+		assert.equal(children[0], el3);
+		assert.equal(children[1], el1);
+		assert.equal(children[2], el2);
+	});
 
 	// it('should switch multiple elements', function() {
 
